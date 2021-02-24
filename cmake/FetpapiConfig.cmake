edaad7e3ea37a730af25c52b55b3ed7b88c15b2e
mpoudret <mathieu.poudret@f2i-consulting.com>
2021-02-24 16:56:42 +0100

    Fix

--- cmake/FetpapiConfig.cmake
+++ cmake/FetpapiConfig.cmake
@@ -1,5 +1,7 @@
 include(CMakeFindDependencyMacro)
 
+find_dependency(Threads)
+
 set (AVRO_INCLUDE_DIR AVRO_INCLUDE_DIR-NOTFOUND CACHE PATH "Path to the directory which contains the avro header files")
 if (NOT IS_DIRECTORY ${AVRO_INCLUDE_DIR})
 	message(WARNING "The Avro include dir (AVRO_INCLUDE_DIR variable) does not look to be a valid directory. Please modify it.")
@@ -13,7 +15,6 @@ if (NOT EXISTS ${AVRO_LIBRARY_DEBUG})
 	message(WARNING "The AVRO library (AVRO_LIBRARY_DEBUG variable) does not look to be a valid file. Please modify it.")
 endif ()
 
-# Boost DEPENDENCY
 find_dependency(Boost)
 if (WIN32 AND (Boost_VERSION_MAJOR EQUAL 1) AND (Boost_VERSION_MINOR LESS 74) AND (Boost_VERSION_MINOR GREATER 71))
 		message(WARNING "You may experience min/max issue with this boost version : See https://github.com/boostorg/beast/issues/1980")
